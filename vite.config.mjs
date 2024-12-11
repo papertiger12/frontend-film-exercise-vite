7b53bac36d9fb26bdc5418ced499017e6db5eee8
Ross <ross.myers@goodrx.com>
2024-12-11 07:44:47 -0700

    add vite intellisense

--- vite.config.mjs
+++ vite.config.mjs
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
 // https://vite.dev/config/
+/** @type {import('vite').UserConfig} */
 export default defineConfig({
   plugins: [react()],
   server: {
